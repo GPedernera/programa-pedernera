b6d878a0435dd2a14e84a10594254d9e61198401
GPedernera <125702320+GPedernera@users.noreply.github.com>
2023-03-02 19:07:10 -0300

    Add files via upload

---
+++ clase3consigna1.java
@@ -0,0 +1,16 @@
+package segundoproyecto;
+
+public class clase3consigna1 {
+
+	public static void main(String[] args) {
+// CONSIGNA 1a
+		String txt1 = "Se puede pensar como un array del tipo de dato char aunque veremos que tiene muchas operaciones particulares.";
+		System.out.println(txt1.length());
+		System.out.println(txt1.charAt(0));
+		
+		// CONSIGNA 1b
+		
+		// CONSIGNA 1c	
+	}
+
+}
